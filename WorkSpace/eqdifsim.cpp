d33453943981588390fc7052014529fe78238fa8
Rodrigo Valente <rodrigoavrav@gmail.com>
2014-07-19 13:56:11 -0300

    Adicionado conversão de sistemas discretos para contínuo

--- WorkSpace/eqdifsim.cpp
+++ WorkSpace/eqdifsim.cpp
@@ -35,40 +35,34 @@ EqdifSim<UsedType>::EqdifSim(Matrix<UsedType> AIn, Matrix<UsedType> BIn, Matrix<
 }
 
 template <class UsedType>
-EqdifSim<UsedType>::EqdifSim(Matrix<UsedType> NumdIn, Matrix<UsedType> DendIn, float Time)
-{
-    this->Numd = NumdIn;
-    this->Dend = DendIn;
-    this->SampleTime = Time;
-    tf2ssd();
-    d2c();
-    ss2tfc();
-    this->typeModel = "TFDiscrete";
-}
-
-template <class UsedType>
 EqdifSim<UsedType>::EqdifSim(Matrix<UsedType> NumIn, Matrix<UsedType> DenIn)
 {
-    this->Num = NumIn;
-    this->Den = DenIn;
-//    this->SampleTime = 0.1;
+    this->TransferFunction = new Polynom<UsedType>*[1];
+    for(int i = 0; i < 1; i++)
+        this->TransferFunction[i] = new Polynom<UsedType>[1];
+    this->TransferFunction[0][0] = Polynom<UsedType>(NumIn, DenIn);
+
     tf2ssc();
     c2d(0.1);
     ss2tfd();
+
     this->typeModel = "TFContinuous";
 }
 
 template <class UsedType>
-void EqdifSim<UsedType>::c2d(float Time)
+EqdifSim<UsedType>::EqdifSim(Matrix<UsedType> NumdIn, Matrix<UsedType> DendIn, float Time)
 {
+    this->DiscreteTransferFunction = new Polynom<UsedType>*[1];
+    for(int i = 0; i < 1; i++)
+        this->DiscreteTransferFunction[i] = new Polynom<UsedType>[1];
+    this->DiscreteTransferFunction[0][0] = Polynom<UsedType>(NumdIn, DendIn);
     this->SampleTime = Time;
-    this->Ad.zeros(this->A.getRows(), this->A.getCols());
-    for(int i = 0; i < 4; i++)
-        this->Ad = this->Ad + (1/factorial(i))*(this->A^i)*(pow(this->SampleTime, i));
-    this->Bd = this->A.inv()*(this->Ad - (this->Ad^0))*this->B;
-    this->Cd = this->C;
-    this->Dd = this->D;
-//    this->typeModel = "SSDiscrete";
+
+    tf2ssd();
+    d2c();
+    ss2tfc();
+
+    this->typeModel = "TFDiscrete";
 }
 
 template <class UsedType>
@@ -97,6 +91,143 @@ void EqdifSim<UsedType>::d2c()
     this->D = this->Dd;
 }
 
+
+template <class UsedType>
+void EqdifSim<UsedType>::c2d(float Time)
+{
+    this->SampleTime = Time;
+    this->Ad.zeros(this->A.getRows(), this->A.getCols());
+    for(int i = 0; i < 4; i++)
+        this->Ad = this->Ad + (1/factorial(i))*(this->A^i)*(pow(this->SampleTime, i));
+    this->Bd = this->A.inv()*(this->Ad - (this->Ad^0))*this->B;
+    this->Cd = this->C;
+    this->Dd = this->D;
+    this->typeModel = "SSDiscrete";
+}
+
+template <class UsedType>
+void EqdifSim<UsedType>::tf2ssc()
+{
+    Matrix<UsedType> I, ZeroVector, tempDen( 1, this->TransferFunction[0][0].getDen().getCols()-1);
+
+    I.eye(this->TransferFunction[0][0].getDen().getCols()-2);
+    ZeroVector.zeros( this->TransferFunction[0][0].getDen().getCols()-2, 1);
+    for (int i = 2; i <= this->TransferFunction[0][0].getDen().getCols(); i++)
+        tempDen.add( 1, i-1, -(this->TransferFunction[0][0].getDen()( 1, i)));
+
+    this->A = tempDen||(I|ZeroVector);
+    this->B.zeros(this->A.getRows(), 1);
+    this->B.add( 1, 1, 1);
+
+    if (this->TransferFunction[0][0].getNum().getCols() == 1)
+    {
+        this->C.add( 1, this->B.getRows(), this->TransferFunction[0][0].getNum()( 1, 1));
+        this->D.add( 1, 1, 0);
+    }
+    else if (this->TransferFunction[0][0].getNum().getCols() < this->TransferFunction[0][0].getDen().getCols())
+    {
+        for (int i = this->B.getRows(); i > 1; i--)
+            this->C.add( 1, i, this->TransferFunction[0][0].getNum()( 1, this->TransferFunction[0][0].getNum().getCols() -( this->B.getRows() - i )));
+        this->D.add( 1, 1, 0);
+    }
+
+    else if (this->TransferFunction[0][0].getNum().getCols() == this->TransferFunction[0][0].getDen().getCols())
+    {
+        float B0 = this->TransferFunction[0][0].getNum()( 1, 1);
+
+        for (int i = 1; i <= this->B.getRows(); i++)
+         {
+            this->C.add( 1, i, this->TransferFunction[0][0].getNum()( 1, i+1)- this->TransferFunction[0][0].getDen()( 1, i+1)*B0);
+         }
+        this->D.add( 1, 1,  B0);
+    }
+}
+
+template <class UsedType>
+void EqdifSim<UsedType>::tf2ssd()
+{
+    Matrix<UsedType> I, ZeroVector, tempDen( 1, this->DiscreteTransferFunction[0][0].getDen().getCols()-1);
+
+    I.eye(this->DiscreteTransferFunction[0][0].getDen().getCols()-2);
+    ZeroVector.zeros( this->DiscreteTransferFunction[0][0].getDen().getCols()-2, 1);
+    for (int i = 2; i <= this->DiscreteTransferFunction[0][0].getDen().getCols(); i++)
+        tempDen.add( 1, i-1, -(this->DiscreteTransferFunction[0][0].getDen()( 1, i)));
+
+    this->Ad = tempDen||(I|ZeroVector);
+    this->Bd.zeros(this->Ad.getRows(), 1);
+    this->Bd.add( 1, 1, 1);
+
+    if (this->DiscreteTransferFunction[0][0].getNum().getCols() == 1)
+    {
+        this->Cd.add( 1, this->Bd.getRows(), this->DiscreteTransferFunction[0][0].getNum()( 1, 1));
+        this->Dd.add( 1, 1, 0);
+    }
+    else if (this->DiscreteTransferFunction[0][0].getNum().getCols() < this->DiscreteTransferFunction[0][0].getDen().getCols())
+    {
+        for (int i = this->Bd.getRows(); i > 1; i--)
+            this->Cd.add( 1, i, this->DiscreteTransferFunction[0][0].getNum()( 1, this->DiscreteTransferFunction[0][0].getNum().getCols() -( this->Bd.getRows() - i )));
+        this->Dd.add( 1, 1, 0);
+    }
+
+    else if (this->DiscreteTransferFunction[0][0].getNum().getCols() == this->DiscreteTransferFunction[0][0].getDen().getCols())
+    {
+        float B0 = this->DiscreteTransferFunction[0][0].getNum()( 1, 1);
+
+        for (int i = 1; i <= this->Bd.getRows(); i++)
+         {
+            this->Cd.add( 1, i, this->DiscreteTransferFunction[0][0].getNum()( 1, i+1)- this->DiscreteTransferFunction[0][0].getDen()( 1, i+1)*B0);
+         }
+        this->Dd.add( 1, 1,  B0);
+    }
+}
+
+template <class UsedType>
+void EqdifSim<UsedType>::ss2tfc()
+{
+    Matrix<UsedType> tempB, tempC;
+
+    this->TransferFunction = new Polynom<UsedType>*[this->C.getRows()];
+    for(int i = 0; i < this->C.getRows(); i++)
+        this->TransferFunction[i] = new Polynom<UsedType>[this->B.getCols()];
+
+    for(int i = 1; i <= this->C.getRows(); i++)
+        for(int j = 1; j<= this->B.getCols(); j++)
+        {
+            tempB = this->B.getColumn(j);
+            tempC = this->C.getLine(i);
+
+            this->TransferFunction[i-1][j-1] = Polynom<UsedType>((this->A - tempB*tempC).pol() - this->A.pol(), this->A.pol());
+            this->TransferFunction[i-1][j-1] = this->TransferFunction[i-1][j-1] + this->D(i,j);
+        }
+}
+
+template <class UsedType>
+void EqdifSim<UsedType>::ss2tfd()
+{
+   Matrix<UsedType> tempBd, tempCd;
+
+    this->DiscreteTransferFunction = new Polynom<UsedType>*[this->Cd.getRows()];
+    for(int i = 0; i < this->Cd.getRows(); i++)
+        this->DiscreteTransferFunction[i] = new Polynom<UsedType>[this->Bd.getCols()];
+
+    for(int i = 1; i <= this->Cd.getRows(); i++)
+        for(int j = 1; j<= this->Bd.getCols(); j++)
+        {
+            tempBd = this->Bd.getColumn(j);
+            tempCd = this->Cd.getLine(i);
+
+            this->DiscreteTransferFunction[i-1][j-1] = Polynom<UsedType>((this->Ad - tempBd*tempCd).pol() - this->Ad.pol(), this->Ad.pol());
+            this->DiscreteTransferFunction[i-1][j-1] = this->DiscreteTransferFunction[i-1][j-1] + this->Dd(i,j);
+        }
+
+}
+
+template <class UsedType>
+void EqdifSim<UsedType>::setModel(std::string model)
+{
+    this->typeModel = model;
+}
+
 template <class UsedType>
 void EqdifSim<UsedType>::printSS()
 {
@@ -113,7 +244,7 @@ void EqdifSim<UsedType>::printSS()
         this->Dd.print();
         cout<<endl<<endl;
     }
-    else
+    if (this->typeModel == "SSContinuous")
     {
         cout<<endl<<"A = "<<endl;
         this->A.print();
@@ -131,111 +262,28 @@ void EqdifSim<UsedType>::printSS()
 template <class UsedType>
 void EqdifSim<UsedType>::printTF()
 {
-    int maxSize;
-    char x;
-    Matrix<UsedType> num, den;
+    if(this->typeModel == "TFContinuous")
+        for(int i = 1; i <= this->C.getRows(); i++)
+            for(int j = 1; j <= this->B.getCols(); j++)
+            {
+                this->TransferFunction[i-1][j-1].setVar('s');
+                this->TransferFunction[i-1][j-1].print();
+            }
 
-    if(this->typeModel == "TFContinous")
-    {
-        num = this->Num;
-        den = this->Den;
-        x = 's';
-    }
-    else
-    {
-        num = this->Numd;
-        den = this->Dend;
-        x = 'z';
-    }
-    if(num.length() > den.length())
-        maxSize = num.length();
-    else
-        maxSize = den.length();
-
-    if(den.length() == 0)
-    {
-        if(num.length() >= 2)
-        {
-            for(int i = 0; i < num.length() - 2; i++)
+    if(this->typeModel == "TFDiscrete")
+        for(int i = 1; i <= this->Cd.getRows(); i++)
+            for(int j = 1; j <= this->Bd.getCols(); j++)
             {
-                if(num( 1, i+1) != 0)
-                {
-                    if(num( 1, i+1) != 1)
-                        std::cout << num( 1, i+1);
-                    std::cout << x << '^' << num.length() - i - 1 << ' ';
-                    if(i != num.length() - 3)
-                        std::cout << '+' << ' ';
-                }
+                this->TransferFunction[i-1][j-1].setVar('z');
+                this->DiscreteTransferFunction[i-1][j-1].print();
             }
-            if(num( 1, num.length() - 1)  != 0)
-            {
-                if(num( 1, num.length() - 1) != 1)
-                        std::cout << '+' << ' ' << num( 1, num.length() - 1);
-                std::cout << x << ' ';
-            }
-        }
-        if(num( 1, num.length()) != 0)
-            std::cout << '+' << ' ' << num( 1, num.length()) << '\n';
-
-    }
-    else {
-        if(num.length() >= 2)
-        {
-            for(int i = 0; i < num.length() - 2; i++)
-            {
-                if(num( 1, i+1) != 0)
-                {
-                    if(num( 1, i+1) != 1)
-                        std::cout << num( 1, i+1);
-                    std::cout << x << '^' << num.length() - i - 1 << ' ';
-                    if( i != num.length() - 3)
-                       std::cout << '+' << ' ';
-                }
-            }
-            if(num( 1, num.length() - 1) != 0)
-            {
-                if(num( 1, num.length() - 1) != 1)
-                    std::cout << num( 1, num.length() - 1);
-                std::cout << x << ' ';
-            }
-        }
-        if(num( 1, num.length()) != 0)
-            std::cout << '+' << ' ' << num( 1, num.length()) << '\n';
-
-        for(int i = 0; i < maxSize; i++)
-            std::cout << '-' << '-' << '-' << '-' << '-';
-        std::cout << '\n';
-
-        if(den.length() >= 2)
-        {
-            for(int i = 0; i < den.length() - 2; i++)
-            {
-                if(den( 1, i+1) != 0)
-                {
-                    if(den( 1, i+1) != 1)
-                        std::cout << den( 1, i+1);
-                    std::cout << x << '^' << den.length() - i - 1 << ' ';
-                    if(i != den.length() - 3)
-                       std::cout << '+' << ' ';
-                }
-            }
-            if(den( 1, den.length() - 1)  != 0)
-            {
-                if(den( 1, den.length() - 1)  != 1)
-                   std::cout << '+' << ' ' << den( 1, den.length() - 1);
-                std::cout << x << ' ';
-            }
-        }
-       if(den( 1, den.length())  != 0)
-           std::cout << '+' << ' ' << den( 1, den.length()) ;
-    }
-    std::cout << '\n';
 }
 
 template <class UsedType>
 void EqdifSim<UsedType>::print()
 {
     int a  = this->typeModel.find("SS");
+    cout<<endl<< "The type of the model is " << this->typeModel << endl;
     if (a != -1)
         this->printSS();
     else
@@ -243,145 +291,29 @@ void EqdifSim<UsedType>::print()
 }
 
 template <class UsedType>
-void EqdifSim<UsedType>::tf2ssc()
+Matrix<UsedType> EqdifSim<UsedType>::Simulation(Matrix<UsedType> SystemInput, bool getState)
 {
-    Matrix<UsedType> I, ZeroVector, tempDen( 1, this->Den.getCols()-1);
+    int SampleLength = SystemInput.getCols();
+    Matrix<UsedType> X(this->A.getRows(), SampleLength);
+    Matrix<UsedType> SystemOutput(this->C.getRows(), SampleLength);
 
-    I.eye(this->Den.getCols()-2);
-    ZeroVector.zeros( this->Den.getCols()-2, 1);
-    for (int i = 2; i <= this->Den.getCols(); i++)
-        tempDen.add( 1, i-1, -(this->Den( 1, i)));
-
-    this->A = tempDen||(I|ZeroVector);
-    this->B.zeros(this->A.getRows(), 1);
-    this->B.add( 1, 1, 1);
-
-    if (this->Num.getCols() == 1)
+    for(int i = 1; i <= SampleLength; i++)
     {
-        this->C.add( 1, this->B.getRows(), this->Num( 1, 1));
-        this->D.add( 1, 1, 0);
-    }
-    else if (this->Num.getCols() < this->Den.getCols())
-    {
-        for (int i = this->B.getRows(); i > 1; i--)
-            this->C.add( 1, i, this->Num( 1, this->Num.getCols() -( this->B.getRows() - i )));
-        this->D.add( 1, 1, 0);
+        X.setLine(i+1, this->Ad*X.getColumn(i) + this->Bd*SystemInput.getColumn(i));
+        SystemOutput = this->Cd*X.getLine(i+1) + this->Dd*SystemInput.getColumn(i);
     }
 
-    else if (this->Num.getCols() == this->Den.getCols())
-    {
-        float B0 = this->Num( 1, 1);
-
-        for (int i = 1; i <= this->B.getRows(); i++)
-         {
-            this->C.add( 1, i, this->Num( 1, i+1)- this->Den( 1, i+1)*B0);
-         }
-        this->D.add( 1, 1,  B0);
-    }
+    if(getState == 0)
+        return SystemOutput;
+    else
+        return X;
 }
 
+//---------------------------------------------------------------------------------
 template <class UsedType>
-void EqdifSim<UsedType>::tf2ssd()
+UsedType EqdifSim<UsedType>::factorial(float n)
 {
-    Matrix<UsedType> I, ZeroVector, tempDen( 1, this->Dend.getCols()-1);
-
-    I.eye(this->Dend.getCols()-2);
-    ZeroVector.zeros( this->Dend.getCols()-2, 1);
-    for (int i = 2; i <= this->Dend.getCols(); i++)
-        tempDen.add( 1, i-1, -(this->Dend( 1, i)));
-
-    this->Ad = tempDen||(I|ZeroVector);
-    this->Bd.zeros(this->Ad.getRows(), 1);
-    this->Bd.add( 1, 1, 1);
-
-    if (this->Numd.getCols() == 1)
-    {
-        this->Cd.add( 1, this->Bd.getRows(), this->Numd( 1, 1));
-        this->Dd.add( 1, 1, 0);
-    }
-    else if (this->Numd.getCols() < this->Dend.getCols())
-    {
-        for (int i = this->Bd.getRows(); i > 1; i--)
-            this->Cd.add( 1, i, this->Numd( 1, this->Numd.getCols() -( this->Bd.getRows() - i )));
-        this->Dd.add( 1, 1, 0);
-    }
-
-    else if (this->Numd.getCols() == this->Dend.getCols())
-    {
-        float B0 = this->Numd( 1, 1);
-
-        for (int i = 1; i <= this->Bd.getRows(); i++)
-         {
-            this->Cd.add( 1, i, this->Numd( 1, i+1)- this->Dend( 1, i+1)*B0);
-         }
-        this->Dd.add( 1, 1,  B0);
-    }
-}
-
-template <class UsedType>
-void EqdifSim<UsedType>::ss2tfc()
-{
-    Matrix<UsedType> Temp;
-
-    Temp = this->A - (this->B*this->C);
-    this->Num = Temp.pol();
-    Num.print();
-    this->Den = this->A.pol();
-    Den.print();
-}
-
-template <class UsedType>
-void EqdifSim<UsedType>::ss2tfd()
-{
-    Matrix<UsedType> Temp, I;
-
-    I.eye(this->Ad.getRows());
-
-    Temp = this->A - (this->Bd*this->Cd);
-    this->Numd = Temp.pol();
-    Numd.print();
-    Dend.print();
-//      Temp = this->Cd*((I - this->Ad)^-1)*this->Bd + this->Dd;
-//      Temp.print();
-}
-
-
-//Matrix<UsedType> EqdifSim<UsedType>::sumPoly()
-//{
-//        Matrix<UsedType> ret;
-//        unsigned int maxNum, difNum;
-
-//        if(vNumSize1 > vNumSize2) {
-//            difNum = vNumSize1 - vNumSize2;
-//            maxNum = vNumSize1;
-//            ret = (double*)calloc(vNumSize1, sizeof(double));
-//            for(unsigned int i = 0; i < maxNum; i++) {
-//                if(i >= difNum)
-//                    ret[i] = vNum2[i - difNum] + vNum1[i];
-//                else
-//                    ret[i] = vNum1[i];
-////              ret.print();
-//            }
-//        }
-//        else {
-//            difNum = vNumSize2 - vNumSize1;
-////          std::cout << difNum;
-//            maxNum = vNumSize2;
-//            ret = (double*)calloc(vNumSize2, sizeof(double));
-//            for(unsigned int i = 0; i < maxNum; i++) {
-//                if(i >= difNum)
-//                    ret[i] = vNum2[i] + vNum1[i - difNum];
-//                else
-//                    ret[i] = vNum2[i];
-////              ret.print();
-//            }
-//        }
-//        return ret;
-//}
-template <class UsedType>
-float EqdifSim<UsedType>::factorial(float n)
-{
-    float retval = 1;
+    UsedType retval = 1;
 
     for (int i = n; i > 1; --i)
         retval *= i;
@@ -390,5 +322,5 @@ float EqdifSim<UsedType>::factorial(float n)
 }
 
 //class template EqdifSim<int>;
-class template EqdifSim<float>;
-class template EqdifSim<double>;
+template class EqdifSim <float>;
+template class EqdifSim <double>;
--- WorkSpace/eqdifsim.h
+++ WorkSpace/eqdifsim.h
@@ -1,37 +1,44 @@
 #ifndef EQDIFSIM_H
 #define EQDIFSIM_H
 
-#include "lsim.h"
+#include "polynom.h"
 
 template <class UsedType>
 class EqdifSim
 {
 private:
     float SampleTime, TimeSimulation;
-    Matrix<UsedType> A, B, C, D, Num, Den, Ad, Bd, Cd, Dd, Numd, Dend;
+    Matrix<UsedType> A, B, C, D, Ad, Bd, Cd, Dd;
+    Polynom<UsedType> **TransferFunction, **DiscreteTransferFunction;
     string typeModel;
+
     void tf2ssc();
     void tf2ssd();
     void ss2tfc();
     void ss2tfd();
+
     Matrix<UsedType> sumPoly();
+
 public:
     EqdifSim();
     EqdifSim(Matrix<UsedType> AIn, Matrix<UsedType> BIn, Matrix<UsedType> CIn, Matrix<UsedType> DIn);
     EqdifSim(Matrix<UsedType> AdIn, Matrix<UsedType> BdIn, Matrix<UsedType> CdIn, Matrix<UsedType> DdIn, float Time);
+
     EqdifSim(Matrix<UsedType> NumIn, Matrix<UsedType> DenIn);
     EqdifSim(Matrix<UsedType> NumdIn, Matrix<UsedType> DendIn, float Time);
 
 //    void ss2tf();
+
+    Matrix<UsedType> Simulation(Matrix<UsedType> SystemInput, bool getState = 0);
 
     void printSS();
     void printTF();
     void print();
     void c2d(float Time);
     void d2c();
+    void setModel(std::string model);
 
-    float factorial(float n);
-
+    UsedType factorial(float n);
 };
 
 #endif // EQDIFSIM_H
