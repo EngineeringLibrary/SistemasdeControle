fa1a8e8f390cae1963b114d5a777001a18784cb6
Rodrigo Valente <rodrigoavrav@gmail.com>
2014-07-28 16:12:11 -0300

    Branch criado somente para a gereção da documentação com o doxygen

---
+++ src/external libs/externalfunctions.h
@@ -0,0 +1,18 @@
+#ifndef __EXTERNALFUNCTIONS_H_INCLUDED
+#define __EXTERNALFUNCTIONS_H_INCLUDED
+
+#include <time.h>
+#include <cstdlib>
+//! Biblioteca de Funções Externas
+
+/*!
+        Está biblioteca tem por objetivo definir todas as fun&ccedil&otildees
+    que n&atildeo s&atildeo pertinentes aos outros arquivos e classes.
+*/
+
+//! Fun&ccedil&atildeeo que gera um n&ucutemero aleat&ocuterio.
+
+
+double random();
+
+#endif // EXTERNALFUNCTIONS_H
---
+++ src/optimization libs/pid.h
@@ -0,0 +1,33 @@
+#ifndef __PID_H_INCLUDED
+#define __PID_H_INCLUDED
+
+#include <cmath>
+
+
+template <typename UsedType>
+class PID
+{
+private:
+
+    bool checkUpLim, checkLowLim;
+    UsedType Error, pastError, integralError, derivativeError, Step, kp, ki, kd, upperLimit, lowerLimit, PIDout;
+
+    void antReset();
+
+    void intError();
+    void difError();
+
+
+
+public:
+    PID();
+
+    void setSampleTime(UsedType Time);
+    void setLimits(UsedType upperLimit, UsedType lowerLimit);
+    void setParams(UsedType kp, UsedType ki, UsedType kd);
+
+    UsedType OutputControl(UsedType Reference, UsedType SignalInput);
+
+};
+
+#endif // PID_H
