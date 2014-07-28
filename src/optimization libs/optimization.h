18478123b4486073fca572750a33ccb01d550be5
Rodrigo Valente <rodrigoavrav@gmail.com>
2014-07-28 14:07:33 -0300

    Separação dos arquivos de acordo com as suas funções. Lib de funções de optimização.

---
+++ src/optimization libs/optimization.h
@@ -0,0 +1,27 @@
+#ifndef OPTIMIZATION_H
+#define OPTIMIZATION_H
+
+#include "..\simulation libs\lsim.h"
+
+//Biblioteca incompleta, ainda não funcional.
+
+template <class UsedType>
+class Optimization
+{
+private:
+    Matrix<UsedType> A, B, P, Theta; //Error;
+    Lsim<UsedType> Model;
+    float lambda;
+    int size, nu, ny;
+    long unsigned int p0;
+
+    void initRLS(int nu, int ny, unsigned long int  p0);
+    void setData(Matrix<UsedType> in, Matrix<UsedType> out);
+
+public:
+    Optimization();
+
+    void RLS(int nu, int ny, unsigned long int p0, Matrix<UsedType> in, Matrix<UsedType> out);
+};
+
+#endif // OPTIMIZATION_H
---
+++ src/optimization libs/pso.h
@@ -0,0 +1,40 @@
+#ifndef PSO_H
+#define PSO_H
+
+#include "..\simulation libs\lsim.h"
+
+template <class UsedType>
+class PSO
+{
+private:
+    bool MinMax;
+    int varNum, PopSize, GenSize;
+    double phi1, phi2, omega, Stime;
+    Matrix<UsedType> V, X, P, G, GGen, GnTimes, Xfitness, Pfitness, Gfitness, GfitnessGen, GfitnessnTime, RunTime;
+    Lsim<UsedType> Model;
+
+
+    Matrix<UsedType> Evaluation(Matrix<UsedType> Matrix2Evaluate);
+    void ParticleUpdate();
+    void ParticleEvaluation();
+    void VelocityUpdate();
+    void initAlgorithm();
+    void FitnessUpdate();
+    void FitnessUpdateMin();
+    void FitnessUpdateMax();
+
+public:
+    PSO();
+    PSO(int varNum, int PopSize, int GenSize);
+    PSO(int varNum, int PopSize, int GenSize, double phi1, double phi2);
+    PSO(int varNum, int PopSize, int GenSize, double phi1, double phi2, double omega, bool MinMax);
+    ~PSO();
+
+    void Run();
+    void Run(int nTimes);
+    void setData(Matrix<UsedType> dataIn, Matrix<UsedType> dataOut);
+
+    double getTime();
+};
+
+#endif // PSO_H
