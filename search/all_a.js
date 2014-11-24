d3cc57bd3e61bfe50c3a70c04a44d75b1ed38bed
lordcobisco <andre.dantas@unp.br>
2014-11-24 09:36:53 -0300

    Atualizando HTML

--- search/all_a.js
+++ search/all_a.js
@@ -1,6 +1,8 @@
 var searchData=
 [
+  ['setcolumn',['setColumn',['../class_matrix.html#a532f6ef6c73a89cb65ad587ab94d7abe',1,'Matrix']]],
   ['setlimits',['setLimits',['../class_p_i_d.html#a3ed27364d4dfbc00359befd1023c6841',1,'PID']]],
+  ['setline',['setLine',['../class_matrix.html#a4d1bd93673b400e5282dfdc2eaeac2af',1,'Matrix']]],
   ['setparams',['setParams',['../class_p_i_d.html#a984d1f5b551ea2477ee07818ed32acf2',1,'PID']]],
   ['setsampletime',['setSampleTime',['../class_p_i_d.html#adf714d63b7d5d16125898b97b1cac602',1,'PID']]],
   ['sim',['Sim',['../class_sim.html',1,'']]],
--- search/functions_3.js
+++ search/functions_3.js
@@ -1,5 +1,7 @@
 var searchData=
 [
   ['getcols',['getCols',['../class_matrix.html#ab37ac29b603443da84f18129a8b1aca5',1,'Matrix']]],
+  ['getcolumn',['getColumn',['../class_matrix.html#ab2e2de4a736bb752af047e33cc600acc',1,'Matrix']]],
+  ['getline',['getLine',['../class_matrix.html#a95c22817d23b8b30b25115478e15694a',1,'Matrix']]],
   ['getrows',['getRows',['../class_matrix.html#a1a544492885c8925f6da46e44a896fe3',1,'Matrix']]]
 ];
