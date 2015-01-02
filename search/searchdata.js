9b42805a673cdb9bc6eb60b0e4b1c96a6d8365e9
lordcobisco <andre.dantas@unp.br>
2015-01-02 16:38:13 -0300

    corrigindo nome

--- search/searchdata.js
+++ search/searchdata.js
@@ -1,12 +1,39 @@
 var indexSectionsWithContent =
 {
+  0: "abcdefghiklmnopqrstuvwxyz~",
+  1: "agmopst",
+  2: "acegmopst",
+  3: "acdefgilmnoprstvwz~",
+  4: "abcdefghiklmnopqrstuvxy",
+  5: "f",
+  6: "acdeflmnorst",
+  7: "p",
+  8: "s"
 };
 
 var indexSectionNames =
 {
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "typedefs",
+  6: "related",
+  7: "defines",
+  8: "pages"
 };
 
 var indexSectionLabels =
 {
+  0: "Todos",
+  1: "Classes",
+  2: "Arquivos",
+  3: "Funções",
+  4: "Variáveis",
+  5: "Definições de Tipos",
+  6: "Amigas",
+  7: "Definições e Macros",
+  8: "Páginas"
 };
 
