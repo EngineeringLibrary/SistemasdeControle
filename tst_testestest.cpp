dbd14026e7145553961859f22be342b04d74cdf1
lordcobisco <andre.dantas@unp.br>
2016-11-08 19:55:15 -0300

    Criação de teste unitário para a biblioteca de matrix e reorganização das pastas.

--- tst_testestest.cpp
+++ tst_testestest.cpp
@@ -19,7 +19,7 @@ private Q_SLOTS:
             LinAlg::Matrix<short> A(1);
         }
         LinAlg::Matrix<short> A(1);
-        QVERIFY2(A(1,1) == 1, "Falhou ao comparar o elemento da matriz com valor short");
+        QVERIFY2(A(1,1) == 2, "Falhou ao comparar o elemento da matriz com valor short");
         QVERIFY2(sizeof(A(1,1)) == sizeof(short), "Falhou ao comparar o tipo do elemento da matriz com valor short");
     }
 
