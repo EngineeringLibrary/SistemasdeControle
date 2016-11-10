ea03fe5bdf7d1690da571a531a34a9e4ca8c66e0
lordcobisco <andre.dantas@unp.br>
2016-11-10 18:09:07 -0300

    Correções

--- Testes/primitiveLibs/LinAlg/matrix/tst_matrixtest.cpp
+++ Testes/primitiveLibs/LinAlg/matrix/tst_matrixtest.cpp
@@ -1,7 +1,7 @@
 #include <QString>
 #include <QtTest>
 #define testMatrix
-#include "../../../../../headers/primitiveLibs/LinAlg/matrix.h"
+#include "../../../../headers/primitiveLibs/LinAlg/matrix.h"
 
 class MatrixTest : public QObject
 {
