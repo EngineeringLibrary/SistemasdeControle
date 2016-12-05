1d3cd78c415f81ea598cc11de2e1f501e4e71f5d
lordcobisco <andre.dantas@unp.br>
2016-12-05 12:09:18 -0300

    Correção dos testes de polynom
    Problemas ocasionados por mudanças em LinAlg.

--- src/primitiveLibs/polynom.hpp
+++ src/primitiveLibs/polynom.hpp
@@ -624,7 +624,8 @@ LinAlg::Matrix<Type> PolynomHandler::Roots(LinAlg::Matrix<Type> smallPoly)
 
         root = poly_Monic||(LinAlg::Eye<Type> (numSize-2) | LinAlg::Zeros<Type> (numSize-2,1));
 //        std::cout << "roots" << LinAlg::EigenValues(root);
-        return LinAlg::Zeros<Type>(zeroRoots,2)||LinAlg::EigenValues(root);
+        LinAlg::Matrix<Type> Ava, Ave; *(Ava, Ave) = LinAlg::eigen(root);
+        return LinAlg::Zeros<Type>(zeroRoots,2)||Ava;
     }
 }
 
